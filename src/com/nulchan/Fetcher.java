83f5632a2e5e7e86e83eaca40a1cd28cc693c209
notanuser <notanuser@users.noreply.github.com>
2013-08-22 01:28:14 +0300

    Update Fetcher.java

--- src/com/nulchan/Fetcher.java
+++ src/com/nulchan/Fetcher.java
@@ -48,7 +48,7 @@ abstract class Fetcher<T extends PostContainer> implements IFetcher<T> {
 	}
 	/**
 	 * Парсит заданный документ в массив.
-	 * @param Что парсим
+	 * @param html Что парсим
 	 * @return массив сообщений
 	 * @throws BoardException 
 	 */
